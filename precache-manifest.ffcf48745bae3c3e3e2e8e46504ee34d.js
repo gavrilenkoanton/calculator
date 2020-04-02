d14c69606e0968f4fd5e7bba0796bc8448e7729a
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-04-02 13:17:39 +0300

    Updates

---
+++ precache-manifest.ffcf48745bae3c3e3e2e8e46504ee34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc59aec452560c5e1043286abb8de094",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "55988d1e824909a2d8e2",
+    "url": "/calculator/static/css/main.f89d863c.chunk.css"
+  },
+  {
+    "revision": "1fb04a1a840879d88a9e",
+    "url": "/calculator/static/js/2.71862116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.71862116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55988d1e824909a2d8e2",
+    "url": "/calculator/static/js/main.6792519f.chunk.js"
+  },
+  {
+    "revision": "8b35fe22c8d8e283127a",
+    "url": "/calculator/static/js/runtime-main.ad4ca43e.js"
+  }
+]);
